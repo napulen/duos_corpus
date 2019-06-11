214c5d76fc44d9321ecb7d369cae5766cae7b306
Nestor Napoles Lopez <napulen@gmail.com>
2019-06-10 20:54:33 -0400

    New: Output a tsv of the voice_crossings

--- voice_crossings.py
+++ voice_crossings.py
@@ -1,11 +1,11 @@
 # Copyright 2019 Nestor Napoles Lopez
-
+#
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
 #    You may obtain a copy of the License at
-
+#
 #        http://www.apache.org/licenses/LICENSE-2.0
-
+#
 #    Unless required by applicable law or agreed to in writing, software
 #    distributed under the License is distributed on an "AS IS" BASIS,
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,17 +20,36 @@ if __name__ == '__main__':
     with open(scorelist) as f:
         pathnames = f.readlines()
         pathnames = [f.strip() for f in pathnames]
+        print('file\tvoice1_lowest_note\tvoice1_highest_note\tvoice1_range\tvoice2_lowest_note\tvoice2_highest_note\tvoice2_range\tvoicecrossing')
         for filename in pathnames:
             if not filename.endswith('.xml'):
                 continue
-            print(filename)
             s = music21.converter.parse(filename)
-            highest = music21.note.Note('C1')
-            lowest = music21.note.Note('C8')
+            outer_notes = []
             for p in s.parts:
-                print(p)
+                highest = music21.note.Note('C1')
+                lowest = music21.note.Note('C8')
                 for n in p.flat.notes:
-                    lowest = n if n < lowest else lowest
+                    if type(n) is music21.chord.Chord:
+                        n = min(n)
                     highest = n if n > highest else highest
-            print(highest)
-            print(lowest)
+                    lowest = n if n < lowest else lowest
+                outer_notes.append((lowest, highest))
+            lower_voice, upper_voice = outer_notes[1], outer_notes[0]
+            range_lower_voice = music21.interval.Interval(lower_voice[0], lower_voice[1])
+            range_upper_voice = music21.interval.Interval(upper_voice[0], upper_voice[1])
+            if lower_voice[1] > upper_voice[0]:
+                voicecrossing = music21.interval.Interval(upper_voice[0], lower_voice[1]).name
+            else:
+                voicecrossing = 'No crossing'
+            row = '{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(
+                filename,
+                lower_voice[0].nameWithOctave,
+                lower_voice[1].nameWithOctave,
+                range_lower_voice.name,
+                upper_voice[0].nameWithOctave,
+                upper_voice[1].nameWithOctave,
+                range_upper_voice.name,
+                voicecrossing
+            )
+            print(row)
